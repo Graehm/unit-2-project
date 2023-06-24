4ac50d070daf15109d70e11f81d2b3e8f8e223b3
Graehm <gpatrellfazio@gmail.com>
2023-06-24 17:34:25 -0500

    server buggin cause lines 9/10 in app

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
 mongoose.connect(process.env.MONGO_URI)
-mongoose.connection.once('open', () => {
-    console.log('Oh hey bud!')
-})
+mongoose.connection.once('open', () => console.log('Oh hey bud!'))
 
 app.listen(PORT, () => {
     console.log('Gotcha Bud.')
