32581a0ae4bf28d8a77105ac1e8d00b0f1481fbb
Graehm <gpatrellfazio@gmail.com>
2023-06-26 21:44:18 -0500

    building out controllers and endpoints

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,2 +1,30 @@
 // look at w13 vid 1 7min to see how to make date render correctly 
 // look at w13 vid 2 to see how to incorporate data relation in endpoints
+
+const Post = require('../models/post')
+const { updateMany } = require('../models/user')
+
+exports.createPost = async function (req, res) {
+    try {
+        req.body.user = req.user._id
+        const post = await Post.create(req.body)
+        res.json(post)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.show = async function (req, res) {
+    try {
+        const post = await Post.findOne({_id: req.params.id})
+        res.json(post)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+// show feed? possible to see index of posts from multiple users? 
+
+// edit/update
+
+// delete
--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,13 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-exports.auth = async (req, res, next) =>{
+exports.auth = async (req, res, next) =>{ //middleware function that verifies/assigns auth token 
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const data = jwt.verify(token, process.env.SECRET)
         const user = await User.findOne({_id: data._id})
         if(!user){
-            throw new Error ('Incorrect user or password')
+            throw new Error ('Hey Bud, you happy with that?')
         }
         req.user = user
         next()
@@ -18,4 +18,50 @@ exports.auth = async (req, res, next) =>{
         res.status(401).json({message: error.message})
     }
 } 
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body)
+        await user.save() // save the user which also hash's the password instead of await User and hash pass.
+        const token = await User.generateAuthToken()
+        res.json({user, token})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.loginUser = async(req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email})
+        if(!user || !await bcrypt.compare(req.body.password, user.password)){
+            throw new Error('Hey Bud, your not you')
+        }else{
+            const token = await user.generateAuthToken()
+            res.json({user, token})
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const update = Object.keys(req.body)
+        this.updateUser.forEach(update => req.user[update] = req.body[update])
+        // const user = await User.findOne({_id: req.user._id})
+        await req.user.save()
+        res.json(user)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        await req.user.deleteOne()
+        res.sendStatus(204)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
 
