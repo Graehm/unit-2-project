6677b57f08a766f38501b823785a3722e99d9f89
Graehm <gpatrellfazio@gmail.com>
2023-06-29 14:01:17 -0500

    finding the schema

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,16 +2,16 @@
 // look at w13 vid 2 to see how to incorporate data relation in endpoints
 
 const Post = require('../models/post')
-const User = require('../models/user')
-const { updateMany } = require('../models/user')
+// const User = require('../models/user')
+// const { updateMany } = require('../models/user')
 
 exports.createPost = async function (req, res) {
     try {
         req.body.user = req.user._id
         const post = await Post.create(req.body)
-        req.user.posts?
-        req.user.posts.addToSet({_id: post._id}):
-        req.user.posts = [{_id: post._id}]
+        // req.user.post?
+        // req.user.post.addToSet({_id: post._id}):
+        // req.user.post = [{_id: post._id}]
         await req.user.save()
         res.json(post)
     } catch (error) {
