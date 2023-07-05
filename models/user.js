168ea0f0379fe1bbd25e000f02d12f8589e55f6c
Graehm <gpatrellfazio@gmail.com>
2023-07-05 15:19:27 -0500

    add email field unique true to user CTRL, then re run each endpoint

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
 })
